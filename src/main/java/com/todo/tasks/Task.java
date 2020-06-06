6169e0aa3927c7ccc4a72d1831098ee979f1130f
Vitalii Kotliar <vkotlyar3@gmail.com>
2020-06-07 00:00:26 +0300

    change h2 to mysql

--- src/main/java/com/todo/tasks/Task.java
+++ src/main/java/com/todo/tasks/Task.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 @Entity
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Integer id;
 
     private String title;
